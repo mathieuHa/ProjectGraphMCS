241f6c726464dfa9325c2dee9a757cb00ad4431b
courault <courault.f@gmail.com>
2018-11-28 17:55:39 +0100

    Ford-Fulkerson implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,10 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	    
         Graphe graphe = new Graphe(2.5, 1, Graphe.LAVAL_CENTER, Graphe.LAVAL_DATA);
         //graphe.cleanFiles();  //Pas utile : il suffit de dire qu'on overwrite quand on veut écrire en passant un false -> new FileWriter(fileName, false)
         System.out.println("Size before" + graphe.getListEdge().size());
@@ -25,4 +26,38 @@ public class Main {
         System.out.println("IT WORKS");
 
     }
+	
+	private ArrayList<Node> findPath(Node src, Node dst) {
+		return null;
+	}
+	
+	private float fordF(Node src, Node dst) {
+		float max_flow = 0, path_flow;
+		ArrayList<Node> path = null;
+		Edge edge;
+		
+		// Tant qu'un chemin existe, on augmente le flot
+		while (!(path = findPath(src, dst)).isEmpty()) {
+			
+			path_flow = Float.MAX_VALUE;
+			
+			// On détermine le flot max du chemin : path_flow
+			for (int i = 0; i < path.size()-1; i++) {
+				edge = Graphe.getEdge(path.get(i), path.get(i+1));
+				path_flow = Math.min(path_flow, edge.getFlow());
+			}
+			
+			// On ajoute le flot du chemin au flot global
+			max_flow += path_flow;
+			
+			// Pour chaque edge, on update les capacités résiduelles
+			for (int i = 0; i < path.size()-1; i++) {
+				edge = Graphe.getEdge(path.get(i), path.get(i+1));
+				edge.incResidualCapacity(path.get(i), path_flow);
+				edge.incResidualCapacity(path.get(i+1), -path_flow);
+			}
+		}
+		
+		return max_flow;
+	}
 }
