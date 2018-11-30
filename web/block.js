a832994c47fb5b49bf525ef9e05854b3fc30fe3e
mathieuHa <mathieu.hanotaux@gmail.com>
2018-11-30 19:37:53 +0100

    Correct block

--- web/block.js
+++ web/block.js
@@ -1,7 +1,6 @@
 var iconL = L.icon({
     iconUrl: 'icon.png',
-
-    iconSize:     [38, 95], // size of the icon
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    iconSize:     [45, 70], // size of the icon
+    iconAnchor:   [22, 70], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
+});L.marker([48.075514,-0.763056], {icon: iconL}).addTo(mymap);
